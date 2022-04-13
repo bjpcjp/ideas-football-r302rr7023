95828e8627895fd63ba224ee69a26f2293bc30a5
bjpcjp <bjpcjp@gmail.com>
2022-04-12 21:17:43 -0500

    core layout hooks - not complete

--- cli.sh
+++ cli.sh
@@ -1,17 +1,27 @@
 # cli_notes.sh
 
-printf "rails: "; rails -v
-printf "ruby: "; ruby -v
+#printf "rails: "; rails -v
+#printf "ruby: "; ruby -v
 
 # CORE ROUTING
+# ============
 # rails g controller go home
 
 # REPO SETUP
+# ==========
 #git init
-echo "# ideas-football-r302rr7023" >> README.md
-git add .
-git commit -m "first commit with go/home" 
-git branch -M main
-git remote add origin git@github.com:bjpcjp/ideas-football-r302rr7023.git
-git push -u origin main
+#echo "# ideas-football-r302rr7023" >> README.md
+#git add .
+#git commit -m "first commit with go/home" 
+#git branch -M main
+#git remote add origin git@github.com:bjpcjp/ideas-football-r302rr7023.git
+#git push -u origin main
 
+# CORE LAYOUT
+# ===========
+
+# gem install bootstrap-sass
+# add bootstrap-sass gem to Gemfile
+# bundle install
+
+touch app/assets/stylesheets/custom.scss
